8873cf12adebe90df450667eaffe679ab354fac4
Mariam4324 <ashkmariam@gmail.com>
2023-12-21 12:32:57 +0300

    2 commit

--- js/script.js
+++ js/script.js
@@ -11,29 +11,29 @@ const errorPassword = document.querySelector(".password_error");
 
 form.addEventListener("submit", (ev) => {
   ev.preventDefault();
-
-  if (!validation()) {
-    console.log("error");
-  } else {
-    console.log("submit");
-  }
 });
 
-inputValidation(textInput, errorText);
+textValidation(textInput, errorText);
 telValidation(telInput, errorTel);
 
 let textInputValidation = false;
 
-function inputValidation(inputElem, errorElem) {
-  inputElem.addEventListener("input", (ev) => {
+function textValidation(elem, errorElem) {
+  elem.addEventListener("input", (ev) => {
     errorText.textContent = "";
-    if (ev.target.value.length < ev.target.dataset.minLength) {
+    if (ev.target.value.length === 0) {
+      textInput.classList.add("unsubmit");
+      errorElem.textContent = `обязательно`;
+    } else if (ev.target.value.length < ev.target.dataset.minLength) {
       textInputValidation = false;
       errorElem.textContent = `имя должно быть более ${ev.target.dataset.minLength} символов `;
     } else if (ev.target.value.length > ev.target.dataset.maxLength) {
       textInputValidation = false;
       errorElem.textContent = `имя должно быть меньше ${ev.target.dataset.maxLength} символов`;
+      textInput.classList.add("unsubmit");
     } else {
+      textInput.classList.remove("unsubmit");
+      textInput.classList.add("submit");
       textInputValidation = true;
     }
   });
@@ -43,16 +43,14 @@ function telValidation(elem, errorElem) {
   elem.addEventListener("input", (ev) => {
     errorElem.textContent = "";
     let content = ev.target.value;
-    let phoneNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+"];
+    let phoneNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
     const splittedContent = content.split("");
 
     const filteredContent = splittedContent.filter((num) =>
       phoneNumbers.some((el) => el === num)
     );
-    console.log(splittedContent);
-    console.log(filteredContent);
-    console.log(content);
+
     ev.target.value = filteredContent.join("");
 
     if (ev.target.value.length < ev.target.dataset.minLength) {
